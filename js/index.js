afd01b14016dba1c6a24b139b1593f82cb92ef46
sn1wfy <martinnikolov688@gmail.com>
2024-11-23 11:42:07 +0200

    Index update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-//като правя джаваскрипта да използвам масива за примерна заявка, попълни дадената карта да бъде с дадената екфшт вфефюa
+//
+
+trainData = [
+    { 
+        "routeID": "Something DONT NEED TO DISPLAY OR USE",
+        "routeStart": "Baltimore, MD",
+        "routeEnd": "Washington, DC",
+        "departureTime": "TIMESTEMP",
+        "arrivalTime": "TIMESTEMP2",
+        "bordingTime": "TIMESTEMP3",
+        "routeEstimate": "55", // minutes
+    },
+    {
+        "routeID": "Something DONT NEED TO DISPLAY OR USE",
+        "routeStart": "Washington, DC",
+        "routeEnd": "Baltimore, MD",
+        "departureTime": "TIMESTEMP",
+        "arrivalTime": "TIMESTEMP2",
+        "bordingTime": "TIMESTEMP3",
+        "routeEstimate": "55", // minutes
+    }
+];
