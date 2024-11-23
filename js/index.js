5374c6c82dc42aa04c92b3e256ea35473a5a9eaf
sn1wfy <martinnikolov688@gmail.com>
2024-11-23 09:12:35 +0200

    Basicdesign added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log("Hello, World");
+
+    document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const target = document.querySelector(link.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+
